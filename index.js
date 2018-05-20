b9752c9cf77fc763924f23b87234a8903b83b36d
thunguy@me.com <thunguy@me.com>
2018-05-20 16:23:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ function johnLennonFacts(facts) {
   let i = 0
   while (i < facts.length) {
     exclaim.push(`${facts[i]}!!!`);
+    i++
     i += 1
+    i = i + 1
+  
   } 
   return exclaim
 }
